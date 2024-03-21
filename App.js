9ee39ca504b80684c4396af150b88ab5eaf9b49e
Vince-arch <vincekimani006@gmail.com>
2024-03-21 19:23:47 +0300

    new files

--- App.js
+++
@@ -1,9 +0,0 @@
-const os = require('os')
-
-
-//info about current user
-const user = os.userInfo()
-console.log(user)
-
-//method returns system's uptime in seconds
-console.log(`The system uptime is ${os.uptime()} seconds`)
