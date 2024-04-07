f50c02f72cb994788ad896e8374091b7b7132440
Vince-arch <vincekimani006@gmail.com>
2024-04-07 10:30:00 +0300

    added dependecies

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,11 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    //console.log(req.method)
+    console.log(req.url)
+    res.writeHead(200, {'content-type':'text/html'})
+    res.write('<h1>Home page</h1>')
+    res.end()
+})
+
+server.listen(5000)
