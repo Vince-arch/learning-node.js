2ffb66a3329bcae2c0d9dda932950e9bde54c457
Vince-arch <vincekimani006@gmail.com>
2024-04-15 10:33:09 +0300

    new changes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,11 +1,21 @@
-const express = require('express');
+const express = require('express')
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send('Home page')
-}
-)
 
-app.listen(5000, () => {
+//setup static and middleware
+app.use(express.static('./public'))
+
+//app.get('/', (req,res)=>{
+//res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+//adding static assets
+// SSR
+//})
+
+app.all('*',(req,res)=>{
+    res.status(404).send('page can not be found')
+})
+
+app.listen(5000, ()=>{
     console.log('Server is listening on port 5000...')
 })
